23b1ec05be838215005dc0590f6fe96c077915a2
ninoala <yegor.nino@gmail.com>
2024-05-16 11:10:56 -0400

    styling changes, added a bit of js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//'discover our tours' button scrolling smoothly to a section on the page
+document.getElementById('scroll-to-tours-btn').addEventListener('click', () => {
+  document.getElementById('section-tours').scrollIntoView({ behavior: 'smooth' });
+})
+
+//display a popup with explanation for the buttons/links on the page
+const popup = document.querySelector('.btn-popup');
+const popupLinks = document.querySelectorAll('.popup-link');
+popupLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        popup.style.display = 'flex'; //display the popup overlay
+
+        //clear previous setTimeout if it exists
+        clearTimeout(popup.dataset.timerId);
+        
+        //automatically close the popup after two seconds
+        const timerId = setTimeout(() => {
+            popup.style.display = 'none'; // Hide the popup overlay
+        }, 2000);
+        
+        //store the timerId in a data attribute for reference
+        popup.dataset.timerId = timerId;
+    });
+});
+
+//close btn for the explanation popup
+document.querySelector('.btn-popup__close-btn').addEventListener('click', () => {
+    popup.style.display = 'none'; //hide the popup overlay
+});
+
+//automatically close the popup after two seconds
+setTimeout(() => {
+  document.querySelector('.btn-popup').style.display = 'none';
+}, 3000);
